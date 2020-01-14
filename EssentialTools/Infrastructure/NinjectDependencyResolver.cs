6094fc7ac6e91c63476d43b9fd20e0b25b761492
User <User@LAPTOP-MES>
2020-01-13 20:04:49 -0500

    Add project files.

---
+++ EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Ninject;
+using Ninject.Parameters;
+using Ninject.Syntax;
+using System.Configuration;
+using EssentialTools.Models;
+
+namespace EssentialTools.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernal;
+
+        public NinjectDependencyResolver()
+        {
+            kernal = new StandardKernel();
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernal.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernal.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernal.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            kernal.Bind<IDiscountHelper>()
+                .To<DefaultDiscountHelper>().WithConstructorArgument("discountParam", 50M);
+            kernal.Bind<IDiscountHelper>().To<FlexibleDiscountHelper>()
+                .WhenInjectedInto<LinqValueCalculator>();
+        }
+    }
+}
---
+++ EssentialTools/Models/LinqValueCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace EssentialTools.Models
+{
+    public class LinqValueCalculator : IValueCalculator
+    {
+        private IDiscountHelper discounter;
+
+        public LinqValueCalculator(IDiscountHelper discountParam)
+        {
+            discounter = discountParam;
+        }
+
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return discounter.ApplyDiscount(products.Sum(p => p.Price));
+        }
+    }
+}
